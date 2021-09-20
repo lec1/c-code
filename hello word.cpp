4378333e07ed42c7f919d0227d68f930cd089fe3
lec1 <91055550+lec1@users.noreply.github.com>
2021-09-20 17:08:18 +0800

    Add files via upload

---
+++ hello word.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>//包含一个叫stdio.h的文件，std标准，i:input;o:output(输入输出函数时引用stdio.h) 
+   int main()//int整型，main前面的int表示main函数调用返回一个整型值 
+   {
+   	printf("hello word\n");//printf为打印函数 ；\n为换行 
+   	return 0; //返回，0整数 
+   }
